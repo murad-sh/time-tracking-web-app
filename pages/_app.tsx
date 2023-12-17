00a39d617f5aa86eef51206a0f04dbf6cc45c233
Murad Shahbazov <shahbazov.msh@gmail.com>
2023-12-17 13:07:21 +0100

    Font optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,9 @@ import { NextPage } from 'next';
 import { ReactElement, ReactNode } from 'react';
 import { TimerProvider } from '@/store/timer-context';
 import { TimeTrackProvider } from '@/store/time-track-context';
+import { Inter } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -22,6 +25,13 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
     <SessionProvider session={pageProps.session}>
       <TimerProvider>
         <TimeTrackProvider>
+          <style jsx global>
+            {`
+              html {
+                font-family: ${inter.style.fontFamily};
+              }
+            `}
+          </style>
           {getLayout(<Component {...pageProps} />)}
         </TimeTrackProvider>
       </TimerProvider>
