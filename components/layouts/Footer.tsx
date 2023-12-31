8dcd61618ef56ce2c53ae62f885d5b35a925e752
Murad Shahbazov <shahbazov.msh@gmail.com>
2023-12-31 01:00:52 +0100

    Footer update

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -7,7 +7,9 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.content}>
-        &copy; {currentYear} by Murad Shahbazov. All rights reserved.
+        &copy; {currentYear} by{' '}
+        <a href="https://github.com/murad-sh">Murad Shahbazov</a>. All rights
+        reserved.
       </div>
     </footer>
   );
