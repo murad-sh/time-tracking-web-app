7546e1a21058407d2ddea3b9945e726a4673ebcc
Murad Shahbazov <shahbazov.msh@gmail.com>
2023-12-31 13:55:28 +0100

    favicon added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/clock.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
